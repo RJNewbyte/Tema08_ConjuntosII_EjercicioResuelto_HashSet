aa1bb91e0bdd526e7ae288167e0236342324dc30
RJNewbyte <150549317+RJNewbyte@users.noreply.github.com>
2024-03-17 20:05:19 +0100

    Initial commit

---
+++ src/main/java/tema08/conjuntosii/ejercicioresuelto_hashset/Tema08_ConjuntosII_EjercicioResuelto_HashSet.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tema08.conjuntosii.ejercicioresuelto_hashset;
+
+/**
+* <h1>ejercicioresuelto_hashset</h1>
+ *
+ * Realiza un pequeño programita que pregunte al usuario 5 números diferentes 
+ * (almacenándolos en un HashSet), y que después calcule la suma de los mismos 
+ * (usando un bucle for-each).
+ * 
+ * <h2>Métodos</h2>
+ * <ul>
+ * <li>nombreMetodo: Descripción.</li>
+ * </ul>
+ *
+ * @author RJNewbyte
+ * @version 1.0
+ * @since 2024-03-17
+ */
+public class Tema08_ConjuntosII_EjercicioResuelto_HashSet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
