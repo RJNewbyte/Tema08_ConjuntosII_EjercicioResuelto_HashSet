288a6b6d0a42fbce3506be2bacd7459635743733
RJNewbyte <150549317+RJNewbyte@users.noreply.github.com>
2024-03-17 20:36:02 +0100

    Ejemplo completado

--- src/main/java/tema08/conjuntosii/ejercicioresuelto_hashset/Tema08_ConjuntosII_EjercicioResuelto_HashSet.java
+++ src/main/java/tema08/conjuntosii/ejercicioresuelto_hashset/Tema08_ConjuntosII_EjercicioResuelto_HashSet.java
@@ -1,16 +1,18 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package tema08.conjuntosii.ejercicioresuelto_hashset;
 
+import java.util.Scanner;
+import java.util.HashSet;
+
 /**
-* <h1>ejercicioresuelto_hashset</h1>
+ * <h1>ejercicioresuelto_hashset</h1>
  *
- * Realiza un pequeño programita que pregunte al usuario 5 números diferentes 
- * (almacenándolos en un HashSet), y que después calcule la suma de los mismos 
+ * Realiza un pequeño programita que pregunte al usuario 5 números diferentes
+ * (almacenándolos en un HashSet), y que después calcule la suma de los mismos
  * (usando un bucle for-each).
- * 
+ *
  * <h2>Métodos</h2>
  * <ul>
  * <li>nombreMetodo: Descripción.</li>
@@ -23,6 +25,23 @@ package tema08.conjuntosii.ejercicioresuelto_hashset;
 public class Tema08_ConjuntosII_EjercicioResuelto_HashSet {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner kbd = new Scanner(System.in);
+        int contador = 0;
+        HashSet<Integer> numeros= new HashSet<Integer>();
+        System.out.println("Vamos a sumar 5 números");
+        while (contador < 5) {
+            System.out.println(contador + 1 + ". Introduzca un número:");
+            int num = kbd.nextInt();
+            kbd.nextLine();//limpia buffer
+            numeros.add(num);
+            contador++;
+        }
+
+        int suma=0;
+            for (Integer n : numeros) {
+                suma+=n;
+            }
+        System.out.println("La suma de los 5 números es: " + suma);
     }
+
 }
